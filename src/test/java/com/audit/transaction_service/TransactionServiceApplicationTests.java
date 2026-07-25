52a38a8d6cb951b7f6681804101e230dae68c5ee
Muhammad Hussain <hussainakbani06@gmail.com>
2026-07-26 00:17:13 +0800

    Initial commit

---
+++ src/test/java/com/audit/transaction_service/TransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audit.transaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
